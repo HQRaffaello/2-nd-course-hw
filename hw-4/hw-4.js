225e35935841a5cc4b60b93acb6629f11c1810be
Elnur <132407067+HQRaffaello@users.noreply.github.com>
2023-07-04 14:00:57 +0300

    Gotovo

--- hw-4/hw-4.js
+++ hw-4/hw-4.js
@@ -24,3 +24,60 @@ while (i <= 22) {
     i++;
 }
 */
+
+/* Задание 4
+const obj = {
+    'Коля': '200',
+    'Вася': '300',
+    'Петя': '400',
+}
+
+for (let key in obj) {
+        console.log(`${key} — зарплата ${obj[key]} долларов.`);
+}
+*/
+
+/* Задание 5
+let n = 1000;
+let num = 0;
+while (n >= 50) {
+   n = n/2;
+   num++;
+}
+console.log(n);
+console.log(num);
+*/
+
+/* Заание 6
+let dayNumber = +prompt('Введите число первой пятницы месяца');
+
+while (dayNumber <= 31) {
+    if (1 <= dayNumber && dayNumber <= 3) {
+        console.log(`Сегодня пятница, ${dayNumber}-е число. Необходимо подготовить отчет.`);
+        dayNumber = dayNumber + 7;
+        console.log(`Сегодня пятница, ${dayNumber}-е число. Необходимо подготовить отчет.`);
+        dayNumber = dayNumber + 7;
+        console.log(`Сегодня пятница, ${dayNumber}-е число. Необходимо подготовить отчет.`);
+        dayNumber = dayNumber + 7;
+        console.log(`Сегодня пятница, ${dayNumber}-е число. Необходимо подготовить отчет.`);
+        dayNumber = dayNumber + 7;
+        console.log(`Сегодня пятница, ${dayNumber}-е число. Необходимо подготовить отчет.`);
+    }
+    else if (4 <= dayNumber && dayNumber <= 7) {
+        console.log(`Сегодня пятница, ${dayNumber}-е число. Необходимо подготовить отчет.`);
+        dayNumber = dayNumber + 7;
+        console.log(`Сегодня пятница, ${dayNumber}-е число. Необходимо подготовить отчет.`);
+        dayNumber = dayNumber + 7;
+        console.log(`Сегодня пятница, ${dayNumber}-е число. Необходимо подготовить отчет.`);
+        dayNumber = dayNumber + 7;
+        console.log(`Сегодня пятница, ${dayNumber}-е число. Необходимо подготовить отчет.`);
+    }
+    
+    else {
+        console.log(`На сегодня нет задач.`);
+    }
+
+
+    break;
+}
+*/
